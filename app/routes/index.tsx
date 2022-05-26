80e918b056fb9540933cb929a7f38bc0a8136483
GabrielNBDS <gabriel.nbds@gmail.com>
2022-05-26 17:26:01 -0400

    📦 NEW: Create home page

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,13 +1,52 @@
-import { Switch, useMantineColorScheme } from "@mantine/core";
+import {
+	Box,
+	Button,
+	Container,
+	Group,
+	Text,
+	Title,
+} from '@mantine/core'
+import { Link } from '@remix-run/react'
 
-export default function Index() {
-  const { colorScheme ,toggleColorScheme } = useMantineColorScheme();
+export default function Home() {
+	return (
+		<Box sx={{
+      minHeight: '100vh',
+			backgroundImage: 'url("data:image/svg+xml,%3Csvg width=\'40\' height=\'40\' viewBox=\'6 0 120 120\' xmlns=\'http://www.w3.org/2000/svg\'%3E%3Cg fill=\'%235c5f66\' fill-opacity=\'0.4\' fill-rule=\'evenodd\'%3E%3Ccircle cx=\'3\' cy=\'3\' r=\'3\'/%3E%3Ccircle cx=\'13\' cy=\'13\' r=\'3\'/%3E%3C/g%3E%3C/svg%3E");'
+		}}>
+			<Container sx={{
+				paddingTop: 48,
+				paddingBottom: 48,
+				maxWidth: 800
+			}}>
+				<Group direction="column" position="center">
+					<Group spacing={1} direction="column" position="center">
+						<Title
+							sx={{
+								fontSize: 48,
+								'@media (max-width: 992px)': {
+									fontSize: 36
+								},
+							}}
+							align="center"
+						>
+              RAM Stack
+						</Title>
+						<Text color="dimmed" sx={{ fontSize: 24, textAlign: 'center' }}>
+              Remix + Adonis + Mantine
+						</Text>
 
-  return (
-    <Switch
-      checked={colorScheme === 'dark'}
-      label="Dark theme"
-      onClick={() => toggleColorScheme()}
-    />
-  );
+					</Group>
+
+					<Text sx={{ textAlign: 'center' }}>
+            The RAM Stack is named after the best girl in Re: Zero. Use it to
+            build server-side react apps with a batteries-included backend api
+            and build fast because of the ui kit.
+					</Text>
+
+          <Link to="/login"><Button>Login</Button></Link>
+				</Group>
+			</Container>
+		</Box>
+	)
 }
